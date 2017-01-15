4decc1e22c0845c824bccb0cc20236fefe4e2aea
RubensTen <rubens.prl10@gmail.com>
2017-01-15 01:53:36 -0600

    Mocks para la aplicacion

---
+++ app/mocks/mock-usuarios.ts
@@ -0,0 +1,9 @@
+import { Usuario } from '../models/usuario';
+
+export const USUARIOS: Usuario [] = [
+	{email: 'demo1@demo.com', password: '12345678'},
+	{email: 'demo2@demo.com', password: '12345678'},
+	{email: 'demo3@demo.com', password: '12345678'},
+	{email: 'demo4@demo.com', password: '12345678'},
+	{email: 'demo5@demo.com', password: '12345678'}
+];
