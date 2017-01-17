d81c41a99a416f708b0fdce516ccd9e723ed43f0
RubensTen <rubens.prl10@gmail.com>
2017-01-16 19:03:41 -0600

    Se agrega contenido de template a el dashboard

--- app/components/admin/admin.component.ts
+++ app/components/admin/admin.component.ts
@@ -2,7 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({	
 	selector: 'admin',
-	templateUrl: 'app/templates/admin.html'
+	templateUrl: 'app/templates/admin.html',
+	styleUrls: [
+		'../assets/css/bootstrap.min.css',
+		'../assets/css/bootstrap-responsive.min.css',		
+		'../assets/css/matrix-style.css',
+		'../assets/css/matrix-media.css'
+	]
 })
 
 export class AdminComponent implements OnInit {
